9dd7c622a7d2039488343397ddfdc04408178201
Chinseok Lee <me@pyhub.kr>
2022-10-27 14:07:18 +0900

    blog.post_new 뷰를 함수기반뷰로 구현

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from blog.models import Post
 
 def index(request):
@@ -18,8 +18,28 @@ def single_post_page(request, pk):
 from django.views.generic import CreateView
 from blog.forms import PostForm
 
-post_new = CreateView.as_view(
-    form_class=PostForm,
-    model=Post,
-    success_url="/blog/",
-)
+# post_new = CreateView.as_view(
+#     form_class=PostForm,
+#     model=Post,
+#     success_url="/blog/",
+# )
+
+def post_new(request):
+    # print("request.method =", request.method)
+    # print("request.POST =", request.POST)
+    if request.method == "GET":
+        form = PostForm()
+    else:
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # 유효성 검사에 통과한 값들이 저장된 dict
+            # form.cleaned_data
+            post = form.save()  # ModelForm에서 지원
+            # return redirect("/blog/")
+            # return redirect(f"/blog/{post.pk}/")
+            # return redirect(post.get_absolute_url())
+            return redirect(post)
+
+    return render(request, "blog/post_form.html", {
+        "form": form,
+    })
