9929fe79c21afff9fdc7b7e504ce687c95880fef
hyhyhy38371 <git@github.com:hyhyhy38371>
2022-10-28 14:16:57 +0900

    내 건 아니지만 열심히 해야 되니까 참고하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'blog',
     'diary',
     'single_pages',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#configure-internal-ips
+# django debug tool_bar
+INTERNAL_IPS = ['127.0.0.1']
