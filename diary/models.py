109da5b938c6fd57eb3d4b5359098a7aafb682e5
Chinseok Lee <me@pyhub.kr>
2022-10-28 11:17:16 +0900

    diary 앱을 다른 프로젝트로부터 이관

---
+++ diary/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Memory(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return f"/diary/{self.pk}/"
+
+    class Meta:
+        # 쿼리셋에서 order_by를 지정하지 않았을 때, 사용되는 기본 정렬
+        ordering = ['-id']
